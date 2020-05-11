ae35cbc590763a1122c5959be793fdd056619ef3
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-05-11 16:14:16 +0800

    匹配

--- js/content-script.js
+++ js/content-script.js
@@ -6,8 +6,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 	}
 });
 
+// 注意，必须设置了run_at=document_start 此段代码才会生效 DOMContentLoaded
+document.addEventListener('DOMContentLoaded', function(){
+	// 注入自定义JS
+	injectCustomJs();
+	hkEvent();
+});
+
 function hkEvent(){
-	if(location.href == 'https://sycm.taobao.com/adm/v2/my'){
+	if(location.href.indexOf('https://sycm.taobao.com/adm/v2/my') != -1){
 
 		setTimeout(function () {
 
@@ -16,7 +23,7 @@ function hkEvent(){
 			for(var i=0; i<alist.length; i++){
 				if(alist[i].text == "下载"){
 					let d = new Date().getTime()
-					console.log(d);	
+					console.log(alist[i]);	
 					alist[i].click();
 				}
 			}
@@ -31,80 +38,35 @@ function hkEvent(){
 
 
 		}, 2000);
+	}else if(location.href.indexOf("https://sycm.taobao.com/cc/macroscopic_monitor") != -1){
+
+		setTimeout(function () {
+			
+			var href1 = $("#coreIndexMonitor .op-cc-download");
+			console.log("fffffff",href1[0])
+			
+			setTimeout(function () {
+			
+				var href2 = $("#item-rank .op-cc-download");
+				console.log("vvvvvvvvvv",href2[0])
+				
+				let arr = [href1[0],href2[0]];
+				for(var i =0;i<arr.length;i++){
+					arr[i].click();
+				}
+
+			}, 3000);
+
+		}, 2000);
+
+		
+
+		
+
+		
 	}
 }
 
-// 注意，必须设置了run_at=document_start 此段代码才会生效 DOMContentLoaded
-document.addEventListener('DOMContentLoaded', function(){
-	// 注入自定义JS
-	injectCustomJs();
-	// 给谷歌搜索结果的超链接增加 _target="blank"
-	
-	// if(location.host == 'sycm.taobao.com'){
-
-	// 	setTimeout(function () {
-
-	// 		let alist = $("#report-detail table tbody a");
-			
-	// 		for(var i=0; i<alist.length; i++){
-	// 			if(alist[i].text == "下载"){
-	// 				let d = new Date().getTime()
-	// 				console.log(d);	
-	// 				alist[i].click();
-	// 			}
-	// 		}
-
-
-	// 		let trList = $("#report-detail table tbody tr");
-			
-	// 		for(var i=0; i<trList.length; i++){
-	// 			console.log($(trList[i].getElementsByTagName("td")[0]).find(".report-name").text());
-				
-	// 		}
-
-
-	// 	}, 2000);
-		
-		
-
-		// function fuckBaiduAD()
-		// {
-		// 	if(document.getElementById('my_custom_css')) return;
-		// 	var temp = document.createElement('style');
-		// 	temp.id = 'my_custom_css';
-		// 	(document.head || document.body).appendChild(temp);
-		// 	var css = `
-		// 	/* 移除百度右侧广告 */
-		// 	#content_right{display:none;}
-		// 	/* 覆盖整个屏幕的相关推荐 */
-		// 	.rrecom-btn-parent{display:none;}'
-		// 	/* 难看的按钮 */
-		// 	.result-op.xpath-log{display:none !important;}`;
-		// 	temp.innerHTML = css;
-		// 	console.log('已注入自定义CSS！');
-		// 	// 屏蔽百度推广信息
-		// 	removeAdByJs();
-		// 	// 这种必须用JS移除的广告一般会有延迟，干脆每隔一段时间清楚一次
-		// 	interval = setInterval(removeAdByJs, 2000);
-			
-		// 	// 重新搜索时页面不会刷新，但是被注入的style会被移除，所以需要重新执行
-		// 	temp.addEventListener('DOMNodeRemoved', function(e)
-		// 	{
-		// 		console.log('自定义CSS被移除，重新注入！');
-		// 		if(interval) clearInterval(interval);
-		// 		fuckBaiduAD();
-		// 	});
-		// }
-		// let interval = 0;
-		// function removeAdByJs()
-		// {
-		// 	$('[data-tuiguang]').parents('[data-click]').remove();
-		// }
-		// fuckBaiduAD();
-		// initCustomPanel();
-		// initCustomEventListen();
-	// }
-});
 
 function initCustomPanel()
 {
